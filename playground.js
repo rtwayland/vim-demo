d2f7f7a2363cac99bf19582dc7969a73fb4bd008
Raleigh Wayland <raleigh.wayland@stormapp.com>
2020-11-17 10:47:39 -0700

    Initial playground activities

---
+++ playground.js
@@ -0,0 +1,39 @@
+/* MODES */
+
+/* Command/Normal */
+// Use the h, j, k, and l keys to move left, down, up, and right
+// Use the $ sign to jump to the end of the line
+// Use the 0 to jump to the beginning of the line
+// With your cursor on line 15, test the difference between 0 and ^ (zero and caret)
+
+const nums = [1, 2, 3, 4];
+
+const numStrings = ["one", "two", "three", "four"];
+
+function test() {
+  const ob = {
+    letters: ["a", "b", "c", "d"],
+  };
+}
+
+/* Insert */
+// Use the i or a key to enter insert mode to put a 3 before the 4 in the below array
+
+const nums2 = [1, 2, 4];
+
+// Put your cursor in the middle of the variable definition below
+// Use the I (shift + i) to enter insert mode at the beginning of the line to write 'const'
+// Then hit escape to enter normal mode again.
+// Use the A (shift + a) to enter insert mode at the end of the line to add a line comment
+
+numStrings2 = ["one", "two", "three", "four"];
+
+const numArray = [
+  // With the cursor on the 2, press the O (shift + o) key to open a new line above and add a 1,
+  // Then hit escape to enter normal mode, press j to go back to the 2 line, and press o to open a line below to type 3
+  2,
+];
+
+/* Visual */
+
+const script = document.createElement("script");
